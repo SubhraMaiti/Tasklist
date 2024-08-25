61cbf905628801a442e7c7b84518179d3afe0e84
SubhraMaiti <subhramaiti0@gmail.com>
2024-08-25 18:37:50 +0530

    email address changed

--- send_email.php
+++ send_email.php
@@ -2,8 +2,8 @@
 $to = "subhramaiti0@gmail.com";  // The recipient's email address
 $subject = "Test Email";  // Subject of the email
 $message = "This is a test email sent from PHP.";  // Body of the email
-$headers = "From: sender@example.com" . "\r\n" . // Sender's email address
-           "Reply-To: sender@example.com" . "\r\n" . // Reply-to email address
+$headers = "From: task_alert@subhra-maiti.in" . "\r\n" . // Sender's email address
+           "Reply-To: task_alert@subhra-maiti.in" . "\r\n" . // Reply-to email address
            "X-Mailer: PHP/" . phpversion();  // Additional headers
 
 // Send the email
